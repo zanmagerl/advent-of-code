ae8323cb549cf8cae864d7ba9a069b885323487c
polhec42 <zan.magerl@hotmail.com>
2019-09-10 16:02:27 +0200

    Day 10

--- 2017/day10.c
+++ 2017/day10.c
@@ -36,10 +36,6 @@ void print_list(item* el, int length){
     print_list(el->next, length-1);
 }
 
-int partOne(){
-
-}
-
 item* get_item_of_index(int index, item* el){
     if(index != 0){
         return get_item_of_index(index-1, el->next);
@@ -47,23 +43,149 @@ item* get_item_of_index(int index, item* el){
     return el;
 }
 
-item* reverse_list(int index, item* el){
+item* shift_item(item* it, int length){
+
+    for(int i = 0; i < length; i++){
+        it = it->next;
+    }
+
+    return it;
+}
+
+void reverse_list(item* el, int length){
     
+    item* iter = el;
+    
+    while(length > 1){
+        
+        item* dest = shift_item(iter, length-1);
+
+        int value = iter->value;
+        iter->value = dest->value;
+        dest->value = value;
+        iter = iter->next;
+
+        length -= 2;
+    }
+}
+
+int partOne(int* lengths, int numberOfLengths){
+    
+    int skip_size = 0;
+    item* position = head;
+
+    for(int i = 0; i < numberOfLengths; i++){
+        
+        reverse_list(position, lengths[i]);
+        
+        int shift = lengths[i] + skip_size;
+        
+        position = shift_item(position, shift);
+        
+        skip_size++;
+    }
+
+    return head->value * head->next->value;
+}
+
+char* partTwo(int* lengths, int number_of_lengths){
+
+    int skip_size = 0;
+    item* position = head;
+    //creating sparse hash
+    for(int step = 0; step < 64; step++){
+
+        for(int i = 0; i < number_of_lengths; i++){
+            
+            reverse_list(position, lengths[i]);
+            
+            int shift = lengths[i] + skip_size;
+            
+            position = shift_item(position, shift);
+            
+            skip_size++;
+        }
+    }
+
+    //creating dense hash
+
+    int* dense_hash = malloc(16 * sizeof(int));
+
+    for(int i = 0; i < 16; i++){
+
+        int value = (shift_item(head, i*16))->value;
+
+        for(int j = 1; j < 16; j++){
+            value ^= (shift_item(head, i*16 + j))->value;
+        }
+        dense_hash[i] = value;
+    }
+
+    //conversion to hash
+    char* knot_hash = malloc(33 * sizeof(char));
+    knot_hash[32] = '\0';
+
+    for(int i = 0; i < 16; i++){
+
+        int value = dense_hash[i];
+
+        int first = value / 16;
+        int second = value % 16;
+
+        if(first < 10){
+            knot_hash[i*2] = first + '0';
+        }else{
+            knot_hash[i*2] = first - 10 + 'a';
+        }
+
+        if(second < 10){
+            knot_hash[i*2 + 1] = second + '0';
+        }else{
+            knot_hash[i*2 + 1] = second - 10 + 'a';
+        }
+    }
+
+    return knot_hash;
 }
 
 int main(){
-    /*
-    int* lengths = malloc(4 * sizeof(int));
-    lengths[0] = 3;
-    lengths[1] = 4;
-    lengths[2] = 1;
-    lengths[3] = 5;*/
+    
+    int number_of_lengths = 16;
+    int* lengths = malloc(number_of_lengths * sizeof(int));
+    
+    FILE* file = fopen("input10.txt", "r");
 
-    createList(0, 5);
+    for(int i = 0; i < number_of_lengths; i++){
+        fscanf(file, "%d", &lengths[i]);
+        fgetc(file);
+    }
+    fclose(file);
 
-    print_list(head, 5);
+    createList(0, 256);
+    
+    printf("%d\n", partOne(lengths, number_of_lengths));
+    free(lengths);
 
-    //printf("%d\n", partOne());
+    file = fopen("input10.txt", "r");
+
+    lengths = malloc(100 * sizeof(int));
+
+    char c = ' ';
+    int i;
+    for(i = 0; (c = fgetc(file)) != EOF; i++){
+        lengths[i] = c;
+    }
+    lengths[i] = 17;
+    lengths[i+1] = 31;
+    lengths[i+2] = 73;
+    lengths[i+3] = 47;
+    lengths[i+4] = 23;
+
+    number_of_lengths = i + 5;
+
+    createList(0, 256);
+
+    printf("%s\n", partTwo(lengths, number_of_lengths));
 
     return 0;
 }
