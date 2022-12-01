d47692f3c105d497e4bedcac5451e645e551bf2b
zanmagerl <zan.magerl@hotmail.com>
2022-12-01 07:33:07 +0100

    Day 01: take advantage of collections

--- 2022/src/twentytwo/days/Day01.kt
+++ 2022/src/twentytwo/days/Day01.kt
@@ -4,37 +4,12 @@ import twentytwo.utilities.readStrings
 
 class Day01 {
 
-    fun partOne(input: List<String>): Int {
-        var mostSum = 0
-        var currentSum = 0
-        for (line in input) {
-            if (line.isBlank()) {
-                if (currentSum > mostSum) {
-                    mostSum = currentSum
-                }
-                currentSum = 0
-            } else {
-                currentSum += line.toInt()
-            }
-        }
-        return mostSum
+    fun partOne(elvesBackpack: List<List<Int>>): Int {
+        return elvesBackpack.maxOf { it.sum() }
     }
 
-    fun partTwo(input: List<String>): Int {
-        val mostSum = mutableListOf(0,0,0)
-        var currentSum = 0
-        for (line in input) {
-            if (line.isBlank()) {
-                val minElement = mostSum.minOrNull()!!
-                if (currentSum > minElement) {
-                    mostSum[mostSum.indexOf(minElement)] = currentSum
-                }
-                currentSum = 0
-            } else {
-                currentSum += line.toInt()
-            }
-        }
-        return mostSum.sum()
+    fun partTwo(elvesBackpack: List<List<Int>>): Int {
+        return elvesBackpack.map { it.sum() }.sortedDescending().subList(0, 3).sum()
     }
 }
 
@@ -43,8 +18,17 @@ fun main(){
     val dayOne = Day01()
 
     val input = readStrings()
+    val elvesBackpacks = mutableListOf<List<Int>>()
+    var currentBackpack = mutableListOf<Int>()
+    for (sandwich in input) {
+        if (sandwich.isBlank()) {
+            elvesBackpacks.add(currentBackpack)
+            currentBackpack = mutableListOf()
+        } else {
+            currentBackpack.add(sandwich.toInt())
+        }
+    }
 
-    println(dayOne.partOne(input))
-    println(dayOne.partTwo(input))
-
+    println(dayOne.partOne(elvesBackpacks))
+    println(dayOne.partTwo(elvesBackpacks))
 }
